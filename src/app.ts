928f4af65e6d1bec73481b72b507aaa8b38e5cfb
Nweke Chinedu Alex <nwekealexchinedu@gmail.com>
2022-06-24 19:33:17 +0100

    added the google maps types

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ const GOOGLE_API_KEY = "AIzaSyCE9YECvTRnjVnvGlOSj8lypPR8m75DU7U";
 
 type GoogleGeocodingResponse = {
   results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS" | "REQUEST_DENIED";
 };
 
 function searchAddressHandler(event: Event) {
@@ -24,10 +25,24 @@ function searchAddressHandler(event: Event) {
       )}&key=${GOOGLE_API_KEY}`
     )
     .then((res) => {
-      console.log("Logging response", res);
+      if (res?.data?.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
       const coordinates = res?.data?.results[0].geometry?.location;
+      const map = new google.maps.Map(
+        document.getElementById("map") as HTMLElement,
+        {
+          center: coordinates,
+          zoom: 16,
+        }
+      );
+      new google.maps.Marker({
+        position: coordinates,
+        map: map,
+      });
     })
     .catch((err) => {
+      alert(err?.message);
       console.log("Logging error", err);
     });
 }
