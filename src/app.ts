bc16d5bb521939755ba0d80ed89a3d4ee2318c50
Nweke Chinedu Alex <nwekealexchinedu@gmail.com>
2022-06-24 18:40:50 +0100

    created a custom type for the google geocoding api response

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,10 @@ const addressInput = document.getElementById("address") as HTMLInputElement;
 
 const GOOGLE_API_KEY = "AIzaSyCE9YECvTRnjVnvGlOSj8lypPR8m75DU7U";
 
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+};
+
 function searchAddressHandler(event: Event) {
   event.preventDefault();
 
@@ -13,11 +17,19 @@ function searchAddressHandler(event: Event) {
 
   // send to Google API
 
-  axios.get(
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
-      enteredAddress
-    )}&key=${GOOGLE_API_KEY}`
-  );
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((res) => {
+      console.log("Logging response", res);
+      const coordinates = res?.data?.results[0].geometry?.location;
+    })
+    .catch((err) => {
+      console.log("Logging error", err);
+    });
 }
 
 form.addEventListener("submit", searchAddressHandler);
