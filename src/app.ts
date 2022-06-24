da8ea3a4839ccf281b14b28ebd1e4a22e712fed1
Nweke Chinedu Alex <nwekealexchinedu@gmail.com>
2022-06-24 18:09:18 +0100

    started creating the function for the address

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
 import "./app.css";
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address") as HTMLInputElement;
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+
+  const enteredAddress = addressInput.value;
+
+  // send to Google API
+}
+
+form.addEventListener("submit", searchAddressHandler);
